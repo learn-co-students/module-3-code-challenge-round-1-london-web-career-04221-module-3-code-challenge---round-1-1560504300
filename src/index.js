8d2d5ca299c43c67a6fc9fce1e450b11488db81e
Lauren Carne <oreocookie@hotmail.co.uk>
2019-06-14 11:39:32 +0100

    Complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,93 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
+document.addEventListener("DOMContentLoaded", () => {
+  fetchAndRenderPage();
+});
+//////////////////////////////////
+let imageId = 2833;
+const imageURL = `https://randopic.herokuapp.com/images/${imageId}`;
+const likeURL = `https://randopic.herokuapp.com/likes/`;
+const commentsURL = `https://randopic.herokuapp.com/comments/`;
+const image = document.querySelector("#image");
+const imageName = document.querySelector("#name");
+const likes = document.querySelector("#likes");
+const likeBtn = document.querySelector("#like_button");
+const form = document.querySelector("#comment_form");
+const ul = document.querySelector("#comments");
+//////////////////////////////////
+function fetchPicture() {
+  return fetch(imageURL).then(response => response.json());
+}
+//////////////////////////////////
+function renderPage(json) {
+  ul.innerHTML = "";
+  image.src = json.url;
+  imageName.innerText = json.name;
+  likes.innerText = json.like_count;
 
-  let imageId = 1 //Enter the id from the fetched image here
+  json.comments.forEach(function(comment) {
+    const li = document.createElement("li");
+    li.innerText = comment.content;
+    ul.appendChild(li);
+  });
+}
+//////////////////////////////////
+function fetchAndRenderPage() {
+  fetchPicture().then(renderPage);
+}
+//////////////////////////////////
+likeBtn.addEventListener("click", addANewLikeToPage);
+//////////////////////////////////
+function addANewLikeToPage() {
+  event.preventDefault();
 
-  const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+  likes.innerText = Number(likes.innerText) + 1;
 
-  const likeURL = `https://randopic.herokuapp.com/likes/`
+  const newLikes = {
+    like_count: likes.innerText,
+    image_id: imageId
+  };
 
-  const commentsURL = `https://randopic.herokuapp.com/comments/`
+  updateLikesToServer(newLikes).then(fetchAndRenderPage);
+}
+//////////////////////////////////
+function updateLikesToServer(newLikes) {
+  return fetch(likeURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
 
-})
+    body: JSON.stringify(newLikes)
+  });
+}
+//////////////////////////////////
+form.addEventListener("submit", addNewCommentToThePage);
+//////////////////////////////////
+function addNewCommentToThePage() {
+  event.preventDefault();
+
+  const newComment = {
+    content: event.target[0].value,
+    image_id: imageId
+  };
+
+  addCommentToServer(newComment).then(fetchAndRenderPage);
+
+  event.target.reset();
+}
+//////////////////////////////////
+function addCommentToServer(newComment) {
+  return fetch(commentsURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(newComment)
+  });
+}
+//////////////////////////////////
+
+//////////////////////////////////
+
+//////////////////////////////////
+
+//////////////////////////////////
